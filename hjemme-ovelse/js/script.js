50584180a0795ca6605144ef5bcc7e60b79f7217
Andreaesther <158466640+Andreaesther@users.noreply.github.com>
2024-09-03 17:56:16 +0200

    Add files via upload

---
+++ hjemme-ovelse/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*Eksempel fra Introduction - Math.floor()
+
+let tal = 5.83;
+let afrundetTal = Math.floor(tal);
+
+console.log("Oprindeligt tal:", tal);
+console.log("Afrundet tal med Math.floor:", afrundetTal);
+*/
+
+
+//Eksempel fra Conditionals - else Statement
+
+let tal= 5;
+
+if (tal>0){
+    console.log("Tallet er positivt");
+}
+else if (tal<0){
+    console.log("Tallet er negativt");
+}
+else{
+    console.log("Tallet er nul");
+}
